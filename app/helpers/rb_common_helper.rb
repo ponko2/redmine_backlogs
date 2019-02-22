3609c1f6169e1beeb7445eda83b84421ec77a80b
ayapapa <ayapapajapan@yahoo.co.jp>
2019-02-22 21:18:59 +0900

    Update rb_common_helper.rb

--- app/helpers/rb_common_helper.rb
+++ app/helpers/rb_common_helper.rb
@@ -5,7 +5,7 @@ module RbCommonHelper
   unloadable
 
   include CustomFieldsHelper
-  include RbPartialsHelper
+  #include RbPartialsHelper
 
   def assignee_id_or_empty(story)
     story.new_record? ? "" : story.assigned_to_id
